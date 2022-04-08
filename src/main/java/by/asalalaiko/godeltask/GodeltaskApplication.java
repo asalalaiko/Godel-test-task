248f0411ab2601fd7aca1b203eee6743842ab8f9
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-04-08 17:03:31 +0300

    first commit
    create progect
    add Flyway
    add sketeton https://github.com/java-mastery-brest/simplewebapp

---
+++ src/main/java/by/asalalaiko/godeltask/GodeltaskApplication.java
@@ -0,0 +1,13 @@
+package by.asalalaiko.godeltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodeltaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodeltaskApplication.class, args);
+	}
+
+}
