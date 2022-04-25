45b4c8f3ff92e6b36c2be2e92601dfcc8a96c742
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-04-25 22:08:25 +0300

    propirties to test

--- src/main/java/by/asalalaiko/godeltask/GodeltaskApplication.java
+++ src/main/java/by/asalalaiko/godeltask/GodeltaskApplication.java
@@ -1,7 +1,6 @@
 package by.asalalaiko.godeltask;
 
-import by.asalalaiko.godeltask.dto.Employee;
-import by.asalalaiko.godeltask.dto.Gender;
+
 import by.asalalaiko.godeltask.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -20,28 +19,9 @@ public class GodeltaskApplication {
 		ApplicationContext context = SpringApplication.run(GodeltaskApplication.class, args);
 		EmployeeService employeeService = context.getBean(EmployeeService.class);
 
-
-		Employee employee = new Employee();
-		employee.setFirstName("Peter");
-		employee.setLastName("Petrov");
-		employee.setDepartmentId(8L);
-		employee.setJobTitle("manager");
-		employee.setGender(Gender.MALE.toString());
-		employee.setDataOfBirth(Date.valueOf("1955-01-21"));
-
-
-
-		employeeService.createEmployee(employee);
-
-        System.out.println("Employee record inserted successfully."+employee.toString());
-
         System.out.println("List:");
 		employeeService.findAllEmployees().stream().forEach(s -> System.out.println(s.toString()));
 
-		employeeService.deleteEmployeeById(6L);
-
-		System.out.println("List:");
-		employeeService.findAllEmployees().stream().forEach(s -> System.out.println(s.toString()));
 
 
 	}
